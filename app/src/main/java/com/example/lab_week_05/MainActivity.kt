2985951216243588185c29897bbcf3232060ccf4
Steven717333 <stevenlee313777@gmail.com>
2025-09-28 17:56:38 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.lab_week_05.api.CatApiService
 import retrofit2.*
 import retrofit2.converter.moshi.MoshiConverterFactory
 import com.example.lab_week_05.model.ImageData
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +29,16 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.api_response)
     }
 
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,10 +55,14 @@ class MainActivity : AppCompatActivity() {
             Response<List<ImageData>>) {
                 if(response.isSuccessful){
                     val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    val firstImage = image?.firstOrNull()?.imageUrl.orEmpty()
+                    if (firstImage.isNotBlank()) {
+                        imageLoader.loadImage(firstImage, imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Missing image URL")
+                    }
                     apiResponseView.text = getString(R.string.image_placeholder,
                         firstImage)
-
                 }
                 else{
                     Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
@@ -56,8 +71,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
-
-        }
+    }
 
     companion object{
         const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
